67a75595ff1955e6dbf3f9d54f07e6062b7d2da0
Bruno Magalhães <magalhaesbrunop@gmail.com>
2018-12-02 18:12:08 -0200

    Todo list complete with datasource and delegate methods.

---
+++ Todoey/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Todoey
+//
+//  Created by Bruno Magalhães on 27/11/18.
+//  Copyright © 2018 Cybernetic Company of Milky Way. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    var sender : String = ""
+    var messageBody : String = ""
+    
+}
