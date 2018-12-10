0295af42bbf0c4d79a7799af598aac5f6f278b00
Bruno Magalhães <magalhaesbrunop@gmail.com>
2018-12-10 11:49:04 -0200

    Persisting data using CoreData.

---
+++ Todoey/DataModel.swift
@@ -0,0 +1,9 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by Bruno Magalhães on 05/12/18.
+//  Copyright © 2018 Cybernetic Company of Milky Way. All rights reserved.
+//
+
+import Foundation
